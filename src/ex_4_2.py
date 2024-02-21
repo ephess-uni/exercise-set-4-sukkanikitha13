e9d91d437c9f1e659b3bfb90e1bd91a1a530bdb5
sukkanikitha13 <156538908+sukkanikitha13@users.noreply.github.com>
2024-02-21 11:38:30 -0600

    Update 4.1 and 4.2

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -6,9 +6,9 @@ def logstamp_to_datetime(datestr):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
+    return datetime.strptime(datestr, 'y-m-dTH:M:S')
 
-
+    
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
